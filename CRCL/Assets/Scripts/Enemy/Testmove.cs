91c90df551109ca7cef38930defc57d2d391733f
TSITCL\s162159 <horiuchi199720@gmail.com>
2018-10-24 18:47:16 +0900

    no message

---
+++ CRCL/Assets/Scripts/Enemy/Testmove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Makoto
+{
+    public class Testmove : MonoBehaviour
+    {
+        Rigidbody rb;
+
+        // Use this for initialization
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(new Vector3(-0.03f, 0, 0));
+            }
+            else if(Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(new Vector3(0.03f, 0, 0));
+            }
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(new Vector3(0, 550, 0));
+            }
+        }
+    }
+}
