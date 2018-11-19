974db30168161f28ecf04937626c77ac4292b883
SugiuraMakoto <margin9876@gmail.com>
2018-11-19 23:55:00 +0900

    エネミー4を追加

--- CRCL/Assets/Scripts/Enemy/BulletMove.cs
+++ CRCL/Assets/Scripts/Enemy/BulletMove.cs
@@ -11,8 +11,14 @@ namespace Makoto
         // 速さ
         private float SPEED = 0.1f;
 
+        [SerializeField]
+        [Range(1.0f, 100.0f)]
+        private float RANGE = 10.0f; 
+
         // 状態
         private bool state;
+
+        private Vector3 startPos;
 
         public bool State
         {
@@ -23,13 +29,18 @@ namespace Makoto
         // Use this for initialization
         void Start()
         {
-
+            startPos = transform.position;
         }
 
         // Update is called once per frame
         void Update()
         {
             transform.Translate(new Vector3(-SPEED, 0, 0));
+
+            if((startPos - transform.position).magnitude >= RANGE)
+            {
+                Destroy(transform.gameObject);
+            }
         }
 
         private void OnTriggerEnter(Collider other)
---
+++ CRCL/Assets/Scripts/Enemy/MovePattern3.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Makoto
+{
+    public class MovePattern3 : MonoBehaviour
+    {
+        [SerializeField]
+        [Range(1, 10)]
+        // 振り返る時間
+        private float LOOK_BACK_TIME = 1.0f;
+
+        [SerializeField]
+        [Range(0, 10)]
+        private float FIRING_INTERVAL = 1.0f;
+
+        [SerializeField]
+        private bool LOOK_BACK_STATE = false;
+
+        private Firing firing;
+
+        // 向き
+        private Direction direction;
+
+        private float lastTime;
+
+        private float firingLastTime;
+
+        private float currentTime;
+
+        // Use this for initialization
+        void Start()
+        {
+            firing = GetComponent<Firing>();
+
+            direction = GetComponent<Direction>();
+
+            lastTime = Time.realtimeSinceStartup;
+
+            firingLastTime = Time.realtimeSinceStartup;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            currentTime = Time.realtimeSinceStartup;
+
+            if (LOOK_BACK_STATE)
+            {
+                if (currentTime - lastTime >= LOOK_BACK_TIME)
+                {
+                    if (direction.DIRECTION == Direction.DirectionData.LEFT)
+                    {
+                        direction.DIRECTION = Direction.DirectionData.RIGHT;
+                    }
+                    else
+                    {
+                        direction.DIRECTION = Direction.DirectionData.LEFT;
+                    }
+
+                    lastTime = Time.realtimeSinceStartup;
+                }
+            }
+
+            if (currentTime - firingLastTime >= FIRING_INTERVAL)
+            {
+                firing.FiringBullet();
+
+                firingLastTime = Time.realtimeSinceStartup;
+            }
+        }
+    }
+}
