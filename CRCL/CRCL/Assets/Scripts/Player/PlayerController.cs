4ee8aa76f08dd964071b3280c774ec5009fe7fde
TSITCL\s162159 <horiuchi199720@gmail.com>
2018-11-21 23:49:51 +0900

    no message

--- CRCL/CRCL/Assets/Scripts/Player/PlayerController.cs
+++ CRCL/CRCL/Assets/Scripts/Player/PlayerController.cs
@@ -19,7 +19,6 @@ public class PlayerController : MonoBehaviour
     public Score m_score;          //スコア
     bool m_downFlag;
     bool m_outFlag;
-    bool m_disFlag;
     //操作キー
     public KeyCode UpKey;
     public KeyCode DownKey;
@@ -79,7 +78,6 @@ public class PlayerController : MonoBehaviour
 
         m_currentVec = new Vector3(0, 0, 0);
         m_downFlag = false;
-        m_disFlag = false;
     }
 
     // Update is called once per frame
@@ -143,7 +141,7 @@ public class PlayerController : MonoBehaviour
 
 
         rb.velocity = new Vector3(rb.velocity.x * friction, rb.velocity.y - gravity * Time.deltaTime, 0);
-      
+
     }
 
     //通常状態の処理
@@ -252,10 +250,8 @@ public class PlayerController : MonoBehaviour
 
     void UpdateStateGoal()
     {
-        m_disFlag = true;
-        // SceneManager.LoadScene("ResultScene");
-        state = PLAYER_STATE.GOALED;
-       
+        m_score.AddScore(1000);
+        SceneManager.LoadScene("ResultScene");
     }
 
     void Jump()
@@ -417,7 +413,7 @@ public class PlayerController : MonoBehaviour
         MAX
     }
 
-   public enum PLAYER_STATE
+    enum PLAYER_STATE
     {
         NORMAL,
         ATTACK,
@@ -426,7 +422,6 @@ public class PlayerController : MonoBehaviour
         SWOON,
         PRESSED,
         GOAL,
-        GOALED,
         MAX
     }
 
@@ -588,10 +583,5 @@ public class PlayerController : MonoBehaviour
         Color col = this.GetComponent<Renderer>().material.color;
         col.a = 1;
         return col;
-    }
-
-     public int GetState()
-    {
-        return (int)state;
     }
 }
