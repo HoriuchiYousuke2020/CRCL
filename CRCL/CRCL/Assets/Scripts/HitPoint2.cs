879bab2e7750786305a0b3b86c513e2eae83367b
TSITCL\s162159 <horiuchi199720@gmail.com>
2018-11-23 01:42:59 +0900

    no message

---
+++ CRCL/CRCL/Assets/Scripts/HitPoint2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HitPoint2 : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(player.GetComponent<PlayerController>().GetStatus().GetHp());
+        this.GetComponent<Image>().fillAmount = player.GetComponent<PlayerController>().GetStatus().GetHp();
+
+    }
+}
