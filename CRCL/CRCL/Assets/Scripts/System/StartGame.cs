dc831ddb0660ea6f2101a6f139df95d515b8b670
SugiuraMakoto <margin9876@gmail.com>
2018-11-23 14:44:25 +0900

    タイトルシーンを更新

--- CRCL/CRCL/Assets/Scripts/System/StartGame.cs
+++ CRCL/CRCL/Assets/Scripts/System/StartGame.cs
@@ -3,19 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using GamepadInput;
-public class StartGame : MonoBehaviour {
-    GamepadState keyState;
+public class StartGame : MonoBehaviour
+{
+    //GamepadState keyState;
+    [SerializeField]
+    private PlayerController[] m_players = new PlayerController[4];
+
+    [SerializeField]
+    private GameObject[] HELICOPTER = new GameObject[4];
+
+    private int goalCount;
+
+    private int timer;
+
     // Use this for initialization
-    void Start () {
-		
-	}
+    void Start ()
+    {
+        goalCount = 0;
+        timer = 0;
+    }
 
     // Update is called once per frame
-    void Update() {
-        keyState = GamePad.GetState(0, false);
-        if (Input.GetKeyDown(KeyCode.Space) || GamePad.GetButtonDown(GamePad.Button.Start, 0))
+    void Update()
+    {
+        //keyState = GamePad.GetState(0, false);
+        //if (Input.GetKeyDown(KeyCode.Space) || GamePad.GetButtonDown(GamePad.Button.Start, 0))
+        //{
+        //    SceneManager.LoadScene("StartScene");
+        //}
+
+        goalCount = 0;
+
+        for (int i = 0; i < 4; i++)
         {
-            SceneManager.LoadScene("StartScene");
+            if (m_players[i].GetState() == PlayerController.PLAYER_STATE.GOALED)
+            {
+                goalCount++;
+            }
+        }
+
+        if(goalCount >= 4)
+        {
+            if(timer == 0)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    HELICOPTER[i].GetComponent<Animator>().SetTrigger("Flight");
+                }
+            }
+
+            timer++;
+
+            if(timer > 120)
+            {
+                SceneManager.LoadScene("StartScene");
+            }
         }
     }
 }
