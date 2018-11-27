23e742be5dcc86c48fe57892abca13650f4b34b2
SugiuraMakoto <margin9876@gmail.com>
2018-11-27 10:30:32 +0900

    HPの表示方法を変更

---
+++ CRCL/CRCL/Assets/Scripts/UIController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Canvas canvas;
+    [SerializeField]
+    private Transform targetTfm;
+
+    private RectTransform canvasRectTfm;
+    private RectTransform myRectTfm;
+    private Vector3 offset = new Vector3(0, 1.5f, 0);
+
+    void Start()
+    {
+        canvasRectTfm = canvas.GetComponent<RectTransform>();
+        myRectTfm = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        Vector2 pos;
+
+        switch (canvas.renderMode)
+        {
+
+            case RenderMode.ScreenSpaceOverlay:
+                myRectTfm.position = RectTransformUtility.WorldToScreenPoint(Camera.main, targetTfm.position + offset);
+
+                break;
+
+            case RenderMode.ScreenSpaceCamera:
+                Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(Camera.main, targetTfm.position + offset);
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRectTfm, screenPos, Camera.main, out pos);
+                myRectTfm.localPosition = pos;
+                break;
+
+            case RenderMode.WorldSpace:
+                myRectTfm.LookAt(Camera.main.transform);
+
+                break;
+        }
+    }
+}
