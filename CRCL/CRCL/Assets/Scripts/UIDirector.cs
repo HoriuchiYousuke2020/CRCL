ba0fac92ef53574aac6146ff1c08c60946e7770d
TSITCL\s162159 <horiuchi199720@gmail.com>
2019-01-21 14:49:47 +0900

    完成

--- CRCL/CRCL/Assets/Scripts/UIDirector.cs
+++ CRCL/CRCL/Assets/Scripts/UIDirector.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 namespace Makoto
 {
@@ -11,13 +11,13 @@ namespace Makoto
         private Camera Camera;
 
         [SerializeField]
-        private GameObject[] Player = new GameObject[4];
+        private GameObject[] Players = new GameObject[4];
 
         [SerializeField]
-        private TextMesh[] Score = new TextMesh[2];
+        private Image[] UIImages = new Image[0];
 
         [SerializeField]
-        private TextMesh[] Item = new TextMesh[2];
+        private Text[] UIText = new Text[0];
 
         [SerializeField]
         [Range(0.0f, 255.0f)]
@@ -28,44 +28,47 @@ namespace Makoto
         // Use this for initialization
         void Start()
         {
-            //state = false;
+            state = false;
         }
 
+        /*
+         * 汎用性を持たせるならプレイヤのポジションと
+         * UIのポジション、幅、高さから計算して
+         * 近いオブジェクトを薄くするほうがいいかと
+        */
         // Update is called once per frame
         void Update()
         {
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    if (Camera.transform.position.x + 5 < Player[i].transform.position.x)
-            //    {
-            //        state = true;
+            for (int i = 0; i < Players.Length; i++)
+            {
+                if (Camera.transform.position.y - 3 > Players[i].transform.position.y)
+                {
+                    state = true;
 
-            //        break;
-            //    }
-            //    else
-            //    {
-            //        state = false;
-            //    }
-            //}
+                    break;
+                }
+                else
+                {
+                    state = false;
+                }
+            }
 
-            //if (state)
-            //{
-            //    for (int i = 0; i < 2; i++)
-            //    {
-            //        Score[i].color = new Color(Score[i].color.r, Score[i].color.g, Score[i].color.b, Alpha / 255.0f);
-            //        Item[i].color = new Color(Item[i].color.r, Item[i].color.g, Item[i].color.b, Alpha / 255.0f);
-            //    }
+            if (state)
+            {
+                for (int i = 0; i < UIImages.Length; i++)
+                {
+                    UIImages[i].color = new Color(UIImages[i].color.r, UIImages[i].color.g, UIImages[i].color.b, Alpha / 255.0f);
+                }
 
-            //    state = false;
-            //}
-            //else
-            //{
-            //    for (int i = 0; i < 2; i++)
-            //    {
-            //        Score[i].color = new Color(Score[i].color.r, Score[i].color.g, Score[i].color.b, 1.0f);
-            //        Item[i].color = new Color(Item[i].color.r, Item[i].color.g, Item[i].color.b, 1.0f);
-            //    }
-            //}          
+                state = false;
+            }
+            else
+            {
+                for (int i = 0; i < UIImages.Length; i++)
+                {
+                    UIImages[i].color = new Color(UIImages[i].color.r, UIImages[i].color.g, UIImages[i].color.b, 1.0f);
+                }
+            }
         }
     }
 }
